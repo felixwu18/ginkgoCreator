d33f3341d66a66b95bffcfe394c6d86df8844cf4
felix <372413304felix@gmail.com>
2020-01-16 03:40:08 +0800

    测试滚动条

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -14,7 +14,7 @@
         <scrollPane />
       </el-tab-pane>
       <el-tab-pane label="锚点导航" name="five">
-        <div class="scroll-container">
+        <!-- <div class="scroll-container">
           <div class="jump_menu">
             目录内容1
           </div>
@@ -28,7 +28,67 @@
             目录内容4
           </div>
         </div>
-        <positionNav class="positionNav" />
+        <positionNav class="positionNav" /> -->
+        <div style="float: right; border: solid; height：100px;">
+            <li>1-dsdsd</li>
+            <li>dsdsd</li>
+            <li>dsdsd</li>
+            <li>dsdsd</li>
+            <li>dsdsd</li>
+            <li>dsdsd</li>
+            <li>dsdsd</li>
+            <li>dsdsd</li>
+            <li>dsdsd</li>
+            <li>dsdsd</li>
+            <li>dsdsd</li>
+            <li>dsdsd</li>
+            <li>dsdsd</li>
+            <li>dsdsd</li>
+            <li>dsdsd</li>
+            <li>dsdsd</li>
+            <li>dsdsd</li>
+            <li>dsdsd</li>
+            <li>dsdsd</li>
+            <li>dsdsd</li>
+            <li>dsdsd</li>
+            <li>dsdsd</li>
+            <li>dsdsd</li>
+            <li>dsdsd</li>
+            <li>dsdsd</li>
+            <li>dsdsd</li>
+            <li>dsdsd</li>
+            <li>dsdsd</li>
+            <li>dsdsd</li>
+            <li>dsdsd</li>
+            <li>dsdsd</li>
+            <li>dsdsd</li>
+            <li>dsdsd</li>
+            <li>dsdsd</li>
+            <li>dsdsd</li>
+            <li>dsdsd</li>
+            <li>dsdsd</li>
+            <li>dsdsd</li>
+            <li>dsdsd</li>
+            <li>dsdsd</li>
+            <li>dsdsd</li>
+            <li>dsdsd</li>
+            <li>dsdsd</li>
+            <li>dsdsd</li>
+            <li>dsdsd</li>
+            <li>dsdsd</li>
+            <li>dsdsd</li>
+            <li>41-dsdsd</li>
+        </div>
+
+      </el-tab-pane>
+      <el-tab-pane class="textLeft height" label="iframe" name="six">
+        <!-- <iframe style="float: right; width: 600px;height: 600px" frameborder="yes" vspace="-170" src="https://www.baidu.com"></iframe> -->
+        <div style="float:right;width:630px;height:350px;overflow:hidden;border:0px"> 
+            <iframe style="margin:-810px 0px 0px -10px" src="http://www.sci99.com/" width="960" height="1280" scrolling="no"></iframe>
+        </div> 
+        <div style="float:right; width:800px;height:900px;overflow:hidden;border:0px"> 
+            <iframe style="margin:-320px 0px 0px -10px;" src="http://www.sci99.com/details/monitorall.aspx" width="700" height="1024" scrolling="no"></iframe>
+        </div>
       </el-tab-pane>
   </el-tabs>
     <!-- <p>{{msg}}</p> -->
@@ -36,6 +96,16 @@
 </template>
 
 <script>
+// iframe常用属性:
+// 1.frameborder:是否显示边框，1(yes),0(no)
+// 2.height:框架作为一个普通元素的高度，建议在使用css设置。
+// 3.width:框架作为一个普通元素的宽度，建议使用css设置。
+// 4.name:框架的名称，window.frames[name]时专用的属性。
+// 5.scrolling:框架的是否滚动。yes,no,auto。
+// 6.src：内框架的地址，可以使页面地址，也可以是图片的地址。
+// 7.srcdoc , 用来替代原来HTML body里面的内容。但是IE不支持, 不过也没什么卵用
+// 8.sandbox: 对iframe进行一些列限制，IE10+支持
+
 import sub_ from "./sub_";
 import scrollPane from "@/components/scrollPane";
 import positionNav from "@/components/positionNav";
@@ -76,6 +146,12 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+// div{
+// overflow-x:hidden;
+// overflow-y:auto;
+// max-height:100%;
+// }
+
 @import "~@/views/scss/style/variables";
 @import "~@/views/scss/style/common";
 .home {
@@ -87,17 +163,17 @@ export default {
 .margin {
   margin: 0 20px 20px 230px;
 }
-.height{
+.height {
   height: 700px;
 }
 
 .el-tab-pane {
- height: 70vh;
- overflow: hidden;
+  height: 70vh;
+  overflow: hidden;
   .jump_menu {
     height: 50vh;
   }
-  .positionNav{
+  .positionNav {
     position: fixed;
     top: 209px;
     right: 40px;
@@ -109,4 +185,3 @@ export default {
   overflow: auto;
 }
 </style>
- 
--- src/views/tools/index.vue
+++ src/views/tools/index.vue
@@ -3,6 +3,10 @@
     <h2>相同参数含义,不同字段配置</h2>
     <el-button @click="ceshi">点击ceshi</el-button>
     <el-button @click="arrUnique">数组去重</el-button>
+    <h2>Proxy数据劫持(vue3)</h2>
+    <el-button style="float: right" @click="proxyTest">测试proxy</el-button>
+    <!-- <el-button style="float: right" @click="noMutate">防止数据修改</el-button> -->
+
   </div>
 </template>
 
@@ -17,6 +21,47 @@ export default {
     }
   },  
   methods: {
+    proxyTest() {
+      // defineProperty的替代者 
+      // Proxy代理对象后放回新对象, 读取源对象，不会触发代理对象的get方法， 测试，取源对象和取新对象属性时，对比效果
+      // 能监听整个对象， defineProperty 只能监听对象某个属性
+      // proxy省去 defineProperty 的for in循环， 性能提高
+      // 还能做啥？ 类型校验，真正的私有变量
+      var obj = {
+        a: 1,
+        b: 2
+      }
+     var newObj = new Proxy(obj, {
+        get(target, key, receiver) {
+          console.log(target, key, receiver)
+          return target[key]
+        },
+        set(target, key, value, receiver) {
+          console.log('Reflect.set')
+          return Reflect.set(target.key, value)
+          // return target[key] = value
+        }
+      })
+      // console.log(obj)
+      // console.log(newObj)
+      newObj.a = 666
+      console.log(newObj)
+    },
+    noMutate() {
+      // var obj = {
+      //   novel: '鹿鼎记'
+      // }
+      // console.log(obj.novel);
+      // Object.defineProperty(obj, 'novel', {
+      //   get() {
+      //     return '鹿鼎记'
+      //   }
+      // })
+      // obj.novel = '已修改'
+      // console.log('pre-obj-suf');
+      // console.log(obj.novel);
+      
+    },
     arrUnique() {
       const unique = [...new Set(this.arr1.concat(this.arr2))] // Array.from(new Set(arr1.concat(arr2)))
       console.log('arr1-arr2-unique')
