7c59b4a90a426bfff3c99b216a0016def4720f0c
felix <372413304felix@gmail.com>
2020-07-12 04:33:57 +0800

    tablecheckbox交互

--- src/components/tableCheckbox/index.vue
+++ src/components/tableCheckbox/index.vue
@@ -51,29 +51,35 @@ export default {
               id: 1,
               val: false,
               label: '备选项2',
-              children: [{cc: 1}, {cc: 2}]
+              children: [{cc: 1}, {cc: 2}],
+              father: {aa: 1}
           }, {
               id: 2,
               val: true,
               label: '备选项2',
-              children: [{cc: 3}, {cc: 4}]
+              children: [{cc: 3}, {cc: 4}],
+              father: {aa: 1}
           }],
           cc: [{
               id: 1,
               val: false,
-              label: '备选项3'
+              label: '备选项3',
+              father: {bb: 1}
           }, {
               id: 2,
               val: false,
-              label: '备选项3'
+              label: '备选项3',
+              father: {bb: 1}
           }, {
               id: 3,
               val: true,
-              label: '备选项3'
+              label: '备选项3',
+              father: {bb: 2}
           }, {
               id: 4,
               val: true,
-              label: '备选项3'
+              label: '备选项3',
+              father: {bb: 2}
           }]
         }]
       }
@@ -84,24 +90,50 @@ export default {
           var curArr = row[flag]
         //   自上而下 关联  
         this.toChildrenCheck(curArr, row)   
+        //   自下而上 关联  
+        this.toFatherCheck(curArr, row)   
       },
       toChildrenCheck(curArr, row) {
         curArr.forEach(ele => {
             if(ele.children) {
                 var prop = Object.keys(ele.children[0])[0] // 关联子级的row中prop
                 var idArr = ele.children.map(child => child[prop]) // 父级关联的子级id数组
-                console.log(prop, 'prop');
-                console.log(row[prop], 'row[prop]');
                 row[prop].forEach(son => { // 子级数组处理
                     if (ele.val) {
                         idArr.includes(son.id) && (son.val = true)
                     } else {
                         idArr.includes(son.id) && (son.val = false)
                     }
-                    console.log(son.children, 'son.children');
                     /* 子级是否有children */
                     if (son.children) {
                         this.toChildrenCheck(row[prop], row) // 当前子级做下级子级的curArr
+                    }
+              })
+            }
+        });
+      },
+      toFatherCheck(curArr, row) {
+        curArr.forEach(ele => {
+            if(ele.father) {
+                var prop = Object.keys(ele.father)[0] // 关联父级的row中prop
+                var fatherId = ele.father[prop] // 子级中关联的父级id
+                row[prop].forEach(father => { // 父级数据处理
+                    if (!ele.val) {
+                        father.id === fatherId && (father.val = false)
+                    }
+                    // else {
+                    //   var curSelect = []
+                    //   var sameFatherCur = []
+                    //   curArr.forEach(cur => {
+                    //       fatherId === cur.father.id && (sameFatherCur.push(cur))
+                    //       fatherId === cur.father.id && ele.val && (curSelect.push(cur))
+                    //   })
+                    //   curSelect.length === sameFatherCur.length ? father.id === fatherId && (father.val = true) : father.val = false
+                    // //   curSelect.length === sameFatherCur.length && father.id === fatherId && (father.val = true)
+                    // }
+                    /* 子级是否有children */
+                    if (father.father) {
+                        this.toFatherCheck(row[prop], row) // 当前父级做上级父级的curArr
                     }
               })
             }
