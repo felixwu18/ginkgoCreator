7f4c7f81e7a32c2c6893923a248897a4376656c2
felixwu18 <372413304@qq.com>
2022-07-09 10:47:22 +0800

    对象数组去重最终版

--- src/utils/index.js
+++ src/utils/index.js
@@ -247,7 +247,22 @@ export function removeClass(ele, cls) {
 }
 
 /**
- * 对象数组去重(重复对象,可将指定字段值累加)
+ * 对象数组去重1
+ * Dedupes array based on criterion returned from iteratee function
+ * Ex: uniqueBy([{id: 1}, {id: 1}, {id: 2}],
+ *     val => val.id
+ * ) = [{id: 1}, {id: 2}]
+ */
+ export function uniqueBy(arr, fun) {
+  const valHas = {}
+ return arr.filter(ele => {
+   const val = fun(ele)
+   return !(val in valHas) && (valHas[val] = 1);
+ })
+}
+
+/**
+ * 对象数组去重2(重复对象,可将指定字段值累加)
  * @param {ObjArr} objArr
  * @param {string} propStr
  */
